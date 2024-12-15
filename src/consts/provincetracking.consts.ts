cbfe3a19a7ecd4bbe3444468bb8e8ccd04b55a82
dj2999 <90122687+dj2999@users.noreply.github.com>
2024-12-14 21:27:57 -0600

    Update provincetracking.consts.ts

--- src/consts/provincetracking.consts.ts
+++ src/consts/provincetracking.consts.ts
@@ -1,12 +1,13 @@
 import { DEFAULT_WEATHER_STATION_ID } from "./server.consts";
 
 export const PROVINCE_TRACKING_DEFAULT_STATIONS = [
-  { name: "Winnipeg", code: `MB/${DEFAULT_WEATHER_STATION_ID}` },
-  { name: "Portage", code: "MB/s0000626" },
-  { name: "Brandon", code: "MB/s0000492" },
-  { name: "Dauphin", code: "MB/s0000508" },
-  { name: "Kenora", code: "ON/s0000651" },
-  { name: "Thompson", code: "MB/s0000695" },
+  { name: "Edmonton", code: `AB/${DEFAULT_WEATHER_STATION_ID}` },
+  { name: "Jasper", code: "AB/s0000218" },
+  { name: "Calgary", code: "AB/s0000047" },
+  { name: "Lethbridge", code: "AB/s0000652" },
+  { name: "Grande Prairie". code: "AB/s0000661" },
+  { name: "High Level", code: "AB/s0000621" },
+  { name: "Saskatoon", code: "SK/s0000797" },
 ];
 
 export const PROVINCE_TRACKING_TEMP_TO_TRACK = {
